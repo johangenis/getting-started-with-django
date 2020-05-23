8a7995bb4caa95528c58daf8bf0632ba54615fca
johangenis <johan.j.genis@gmail.com>
2020-05-23 20:59:31 +0200

    Created Car class with primary key name.

--- starter_code_1-models/src/users/models.py
+++ starter_code_1-models/src/users/models.py
@@ -17,8 +17,12 @@ STATUS_CHOICES = {
 
 class Website(models.Model):
     name = models.CharField(max_length=50)
-    url = models.URLField()
+    url = models.URLField(unique=True)
     release_date = models.DateField()
     rating = models.IntegerField()
     status = models.CharField(choices=STATUS_CHOICES, max_length=1)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Car(models.Model):
+    name = models.CharField(max_length=40, primary_key=True)
