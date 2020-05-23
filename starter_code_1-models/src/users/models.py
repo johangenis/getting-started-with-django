c36b5428b80319cb951103b2abd6d9f45c68e97e
johangenis <johan.j.genis@gmail.com>
2020-05-23 20:45:52 +0200

    Added field options to Website model.

--- starter_code_1-models/src/users/models.py
+++ starter_code_1-models/src/users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 
 class User(models.Model):
@@ -6,9 +7,18 @@ class User(models.Model):
     last_name = models.CharField(max_length=30)
 
 
+STATUS_CHOICES = {
+    ("R", "Reviewed"),
+    ("N", "Not reviewed"),
+    ("E", "Error"),
+    ("A", "Accepted"),
+}
+
+
 class Website(models.Model):
     name = models.CharField(max_length=50)
     url = models.URLField()
     release_date = models.DateField()
     rating = models.IntegerField()
+    status = models.CharField(choices=STATUS_CHOICES, max_length=1)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
