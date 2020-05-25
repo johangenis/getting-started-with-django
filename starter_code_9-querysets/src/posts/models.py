56eb3d52a34edb6a242e6df9033f22ea916c6037
johangenis <johan.j.genis@gmail.com>
2020-05-25 13:49:51 +0200

    Added related_name=entries to Entry.authors field

--- starter_code_9-querysets/src/posts/models.py
+++ starter_code_9-querysets/src/posts/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 class Blog(models.Model):
     name = models.CharField(max_length=100)
     tagline = models.TextField()
 
     def __str__(self):
         return self.name
+
 
 class Author(models.Model):
     name = models.CharField(max_length=200)
@@ -14,13 +16,14 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
+
 class Entry(models.Model):
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
     headline = models.CharField(max_length=255)
     body_text = models.TextField()
     pub_date = models.DateField()
     mod_date = models.DateField()
-    authors = models.ManyToManyField(Author)
+    authors = models.ManyToManyField(Author, related_name="entries")
     n_comments = models.IntegerField()
     n_pingbacks = models.IntegerField()
     rating = models.IntegerField()
